0517228959f8c17812dafd9a99e11468baff4954
Ziqin Zhang <ziqin.zhang@tum.de>
2025-02-15 22:08:32 +0100

    implement compress functions

--- llava/model/compress_functions.py
+++ llava/model/compress_functions.py
@@ -1,16 +1,16 @@
-#    Copyright 2024 Flash-VStream Authors 
+#    Copyright 2024 Flash-VStream Authors
 #
-#    Licensed under the Apache License, Version 2.0 (the "License"); 
-#    you may not use this file except in compliance with the License. 
-#    You may obtain a copy of the License at 
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
 #
-#        http://www.apache.org/licenses/LICENSE-2.0 
+#        http://www.apache.org/licenses/LICENSE-2.0
 #
-#    Unless required by applicable law or agreed to in writing, software 
-#    distributed under the License is distributed on an "AS IS" BASIS, 
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-#    See the License for the specific language governing permissions and 
-#    limitations under the License. 
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
 
 import random
 import torch
@@ -42,10 +42,10 @@ def drop_feature(img_feature, video_max_frames, img_similarity=None):
         cur_feature = torch.cat([all_feature[:idx], all_feature[idx + 1:]])
         if idx + 1 == T0 + 1:
             cur_sim = all_sim[:T0 - 1]
-            cur_indices = all_indices[:-1] 
+            cur_indices = all_indices[:-1]
         elif idx == 0:
             cur_sim = all_sim[1:]
-            cur_indices = all_indices[1:] 
+            cur_indices = all_indices[1:]
         else:
             cur_sim = torch.cat([all_sim[:idx], all_sim[idx + 1:]])
             cur_sim[idx - 1] = F.cosine_similarity(all_feature[idx - 1].view(-1), all_feature[idx + 1].view(-1), dim=0)
@@ -117,7 +117,7 @@ def kmeans_feature(img_feature, video_max_frames, img_similarity=None):
     T0 = video_max_frames
     if T <= T0:
         return img_feature, img_similarity, [[[i] for i in range(T)]]
-    X = img_feature.view(T, -1)  # [T, P, D]
+    X = img_feature.contiguous().view(T, -1)  # [T, P, D]
     centroids, labels, exit_step = kmeans_torch(X, T0)
     reduced_feature = centroids.view(T0, P, D)
     # print(f'Note: perform kmeans feature {img_feature.shape} to {reduced_feature.shape}, exit at step={exit_step}')  # actually, K=T0

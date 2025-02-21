c19eebd11d28e15f199e76df7bd361d90ed71d8e
Ziqin Zhang <ziqin.zhang@tum.de>
2025-02-21 20:51:06 +0100

    add segment before encode

--- llava/model/llava_arch.py
+++ llava/model/llava_arch.py
@@ -359,7 +359,7 @@ class LlavaMetaForCausalLM(MultimodalOpsMixin, ABC):
                 # If it is not a video feature, we don't need to process it
                 if idx not in video_idx_in_batch:
                     continue
-                boundaries = segment(image.mean(dim=1), k=50)
+                boundaries = segment(image.mean(dim=1).flatten(1,2), k=50)
                 images_list[idx] = torch.cat([image[boundaries[i]:boundaries[i + 1]] for i in range(len(boundaries) - 1)], dim=0)
 
 

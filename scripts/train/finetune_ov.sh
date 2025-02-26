be348e0d76c0f46ed740a46e6244080c2e4349c2
Ziqin Zhang <ziqin.zhang@tum.de>
2025-02-26 17:04:09 +0100

    debug train script

--- scripts/train/finetune_ov.sh
+++ scripts/train/finetune_ov.sh
@@ -39,7 +39,7 @@ ACCELERATE_CPU_AFFINITY=1 torchrun --nproc_per_node="${NUM_GPUS}" --nnodes="${NN
     --deepspeed scripts/zero3.json \
     --model_name_or_path $PREV_STAGE_CHECKPOINT \
     --version $PROMPT_VERSION \
-    --data_path /mnt/bn/vl-research/workspace/boli01/projects/LLaVA_Next/scripts/i18n/scale_llms/next_ov_stage_july21.yaml \
+    --data_path /home/hpc/b232dd/b232dd16/LLaVA-OV/scripts/train/onevision.yaml \
     --image_folder /anvme/workspace/b232dd21-zyr/llava-data \
     --video_folder /anvme/workspace/b232dd16-LLaVA-OV/llava-video \
     --mm_tunable_parts="mm_vision_tower,mm_mlp_adapter,mm_language_model" \

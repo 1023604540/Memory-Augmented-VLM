af0829be9501213c7952a1efb67a582d56c5b73e
Ziqin Zhang <ziqin.zhang@tum.de>
2025-03-02 18:26:02 +0100

    test train only mlp and memory layer

--- scripts/train/finetune_ov_3.sh
+++ scripts/train/finetune_ov_3.sh
@@ -45,10 +45,10 @@ ACCELERATE_CPU_AFFINITY=0 torchrun --nproc_per_node="${NUM_GPUS}" --nnodes="${NN
     --deepspeed scripts/zero3.json \
     --model_name_or_path $PREV_STAGE_CHECKPOINT \
     --version $PROMPT_VERSION \
-    --data_path /home/hpc/b232dd/b232dd16/LLaVA-OV/scripts/train/sharegpt_train.yaml \
+    --data_path /home/hpc/b232dd/b232dd16/LLaVA-OV/scripts/train/memory_train.yaml \
     --image_folder /anvme/workspace/b232dd21-zyr/llava-data \
     --video_folder /anvme/workspace/b232dd16-LLaVA-OV/llava-video \
-    --mm_tunable_parts="mm_mlp_adapter, mm_language_model" \
+    --mm_tunable_parts="mm_mlp_adapter, attention_model" \
     --mm_vision_tower_lr=2e-6 \
     --vision_tower ${VISION_MODEL_VERSION} \
     --mm_projector_type mlp2x_gelu \

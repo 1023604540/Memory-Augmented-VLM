33d1626c359cea46a923322c7aed5de9b6d7d49a
Ziqin Zhang <ziqin.zhang@tum.de>
2025-03-31 01:11:08 +0200

    test memory inference

--- llava/model/language_model/llava_qwen.py
+++ llava/model/language_model/llava_qwen.py
@@ -161,6 +161,7 @@ class LlavaQwenForCausalLM(Qwen2ForCausalLM, LlavaMetaForCausalLM):
                     memory_mask = torch.ones(B, T_mem, dtype=attention_mask.dtype, device=attention_mask.device)
                     new_attention_mask = torch.cat([memory_mask, attention_mask], dim=1)
                     print(f"new_attention_mask shape, {new_attention_mask.shape}")
+                    kwargs["attention_mask"] = new_attention_mask
 
                 # === 2. Expand cache_position ===
                 if cache_position is not None:
@@ -168,9 +169,10 @@ class LlavaQwenForCausalLM(Qwen2ForCausalLM, LlavaMetaForCausalLM):
                     last_pos_val = cache_position[0].item() + T_mem
                     new_cache_position = torch.tensor([last_pos_val])
                     print(f"new_cache_position shape, {new_cache_position.shape}")
+                    kwargs["cache_position"] = new_cache_position
 
                 # === 3. Inject past_key_values ===
-                # past_key_values = self.inject_memory_as_kv(memory_readout, past_key_values)
+                past_key_values = self.inject_memory_as_kv(memory_readout, past_key_values)
                 # inputs["past_key_values"] = past_key_values
 
                 self.model.memory_readout_cache = None

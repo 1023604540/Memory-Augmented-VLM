e09a10f3b06dbd9245ea4a5acf10409a7802baa2
Ziqin Zhang <ziqin.zhang@tum.de>
2025-03-30 15:48:56 +0200

    test memory inference

--- llava/model/language_model/llava_qwen.py
+++ llava/model/language_model/llava_qwen.py
@@ -147,7 +147,7 @@ class LlavaQwenForCausalLM(Qwen2ForCausalLM, LlavaMetaForCausalLM):
             inputs["image_sizes"] = image_sizes
         print("inputs coming")
         old_cache = inputs.get("past_key_values", None)
-        print("old_cache", len(old_cache))
+        print("old_cache", len(old_cache) if old_cache is not None else None)
         # Inject memory into past_key_values
         # if self.model.memory_readout_cache is not None:
         #     memory_readout = self.model.memory_readout_cache.to(dtype=self.dtype, device=self.device)

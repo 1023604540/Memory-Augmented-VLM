2f03b5a020b868c381b3af52529ee06e1dc39179
Ziqin Zhang <ziqin.zhang@tum.de>
2025-04-06 01:26:38 +0200

    debug

--- llava/model/memory_module/MemoryController.py
+++ llava/model/memory_module/MemoryController.py
@@ -26,11 +26,11 @@ class EpisodicMemoryController:
         original_dtype = query_vec.dtype
         query_vec = query_vec.to(self.compute_dtype)  # (Nq, D)
         cur_memory = self.mem_keys[:self.next_idx].flatten(0, 1)  # (N*P, D)
-        print(f"Memory to be inversed: {cur_memory}")
+        print(f"Memory to be inversed: {cur_memory.shape}")
         memory_inv = torch.linalg.pinv(cur_memory)  # (D, N*P)
         temp = query_vec @ memory_inv  # (Nq, N*P)
         temp_add_noise = self.add_noise(temp, sigma=0.001)  # (Nq, N*P)
-        Z = temp_add_noise @ self.mem_keys  # (Nq, D)
+        Z = temp_add_noise @ cur_memory  # (Nq, D)
         return Z.to(original_dtype)
 
     def integrate(self, old_memory, new_memory):

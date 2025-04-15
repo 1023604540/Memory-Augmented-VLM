ff2fad27dafe8b91ac3a81e4e788c5ad76aecedf
Ziqin Zhang <ziqin.zhang@tum.de>
2025-04-15 22:28:37 +0200

    reset eval settings, move kv injection function

--- llava/train/train.py
+++ llava/train/train.py
@@ -1756,8 +1756,9 @@ def train(attn_implementation=None):
 
     data_module = make_supervised_data_module(tokenizer=tokenizer, data_args=data_args)
     print("Setting up LLavaTrainer...!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    trainer = LLaVAEvalTrainer(model=model, tokenizer=tokenizer, args=training_args, **data_module)
+    # trainer = LLaVAEvalTrainer(model=model, tokenizer=tokenizer, args=training_args, **data_module)
     # trainer = LLaVATrainer(model=model, tokenizer=tokenizer, args=training_args, callbacks=[LogMultipleLrsCallback()], **data_module)
+    trainer = LLaVATrainer(model=model, tokenizer=tokenizer, args=training_args, **data_module)
     # Manually create the optimizer with custom LR groups
     trainer.create_optimizer()
     if list(pathlib.Path(training_args.output_dir).glob("checkpoint-*")):

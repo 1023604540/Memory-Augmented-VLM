a555d9e51724bfb5039d9bfeea9952efcfe37eff
Ziqin Zhang <ziqin.zhang@tum.de>
2025-04-16 02:06:46 +0200

    move kv injection function

--- llava/model/language_model/llava_qwen.py
+++ llava/model/language_model/llava_qwen.py
@@ -95,6 +95,7 @@ class LlavaQwenForCausalLM(Qwen2ForCausalLM, LlavaMetaForCausalLM):
         cache_position=None,
     ) -> Union[Tuple, CausalLMOutputWithPast]:
         print("forward function called !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        print("use_cache", use_cache)
         if inputs_embeds is None:
             (input_ids, position_ids, attention_mask, past_key_values, inputs_embeds, labels) = self.prepare_inputs_labels_for_multimodal(input_ids, position_ids, attention_mask, past_key_values, labels, images, modalities, image_sizes)
         print(f"position_ids", position_ids)

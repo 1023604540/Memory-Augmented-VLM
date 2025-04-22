d727b24c763f5da31945ab3255e1bbf4399ca9d2
Ziqin Zhang <ziqin.zhang@tum.de>
2025-04-23 00:36:41 +0200

    override forward

--- llava/model/language_model/modeling_qwen2.py
+++ llava/model/language_model/modeling_qwen2.py
@@ -1185,7 +1185,7 @@ class Qwen2ForCausalLM(Qwen2PreTrainedModel):
         >>> tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
         "Hey, are you conscious? Can you talk to me?\nI'm not conscious, but I can talk to you."
         ```"""
-        print("Qwen2ForCausalLM.forward")
+        print("Qwen2ForCausalLM.forward, memory_prompt:", memory_prompt)
         output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
         output_hidden_states = (
             output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
@@ -1203,6 +1203,7 @@ class Qwen2ForCausalLM(Qwen2PreTrainedModel):
             output_attentions=output_attentions,
             output_hidden_states=output_hidden_states,
             return_dict=return_dict,
+            memory_prompt=memory_prompt,
         )
 
         hidden_states = outputs[0]

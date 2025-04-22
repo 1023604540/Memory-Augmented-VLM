f945bd14d9e327c85e024291cc7b12fcd30a735c
Ziqin Zhang <ziqin.zhang@tum.de>
2025-04-22 18:05:36 +0200

    override forward

--- llava/model/language_model/llava_qwen.py
+++ llava/model/language_model/llava_qwen.py
@@ -26,8 +26,8 @@ from transformers.generation.utils import GenerateOutput
 
 # from ...constants import IGNORE_INDEX, IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
 from llava.model.llava_arch import LlavaMetaModel, LlavaMetaForCausalLM
-from transformers import Qwen2Config, Qwen2ForCausalLM
-from .modeling_qwen2 import Qwen2Model
+from transformers import Qwen2Config, Qwen2ForCausalLM, Qwen2Model
+# from .modeling_qwen2 import Qwen2Model
 from transformers.utils import logging
 from transformers.modeling_attn_mask_utils import AttentionMaskConverter
 
@@ -45,97 +45,152 @@ class LlavaQwenModel(LlavaMetaModel, Qwen2Model):
 
     def __init__(self, config: Qwen2Config):
         super(LlavaQwenModel, self).__init__(config)
+    def forward(
+        self,
+        input_ids: torch.LongTensor = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_values: Optional[List[torch.FloatTensor]] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        use_cache: Optional[bool] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+    ) -> Union[Tuple, BaseModelOutputWithPast]:
+        print("I am overriding the forward function of Qwen2Model")
+        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+        output_hidden_states = (
+            output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+        )
+        use_cache = use_cache if use_cache is not None else self.config.use_cache
 
-    # Override the forward method to include memory prompts
-    # def forward(
-    #     self,
-    #     input_ids: torch.LongTensor = None,
-    #     attention_mask: Optional[torch.Tensor] = None,
-    #     position_ids: Optional[torch.LongTensor] = None,
-    #     past_key_values: Optional[List[torch.FloatTensor]] = None,
-    #     inputs_embeds: Optional[torch.FloatTensor] = None,
-    #     use_cache: Optional[bool] = None,
-    #     output_attentions: Optional[bool] = None,
-    #     output_hidden_states: Optional[bool] = None,
-    #     return_dict: Optional[bool] = None,
-    #     cache_position: Optional[torch.LongTensor] = None,
-    #     memory_prompt: Optional[torch.FloatTensor] = None,
-    # ) -> Union[Tuple, BaseModelOutputWithPast]:
-    #
-    #     output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
-    #     output_hidden_states = (
-    #         output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
-    #     )
-    #     use_cache = use_cache if use_cache is not None else self.config.use_cache
-    #     return_dict = return_dict if return_dict is not None else self.config.use_return_dict
-    #
-    #     if inputs_embeds is None:
-    #         inputs_embeds = self.embed_tokens(input_ids)
-    #
-    #     if cache_position is None:
-    #         past_seen_tokens = past_key_values.get_seq_length() if past_key_values is not None else 0
-    #         cache_position = torch.arange(
-    #             past_seen_tokens, past_seen_tokens + inputs_embeds.shape[1], device=inputs_embeds.device
-    #         )
-    #     if position_ids is None:
-    #         position_ids = cache_position.unsqueeze(0)
-    #
-    #     causal_mask = self._update_causal_mask(
-    #         attention_mask, inputs_embeds, cache_position, past_key_values, output_attentions
-    #     )
-    #
-    #     hidden_states = inputs_embeds
-    #     position_embeddings = self.rotary_emb(hidden_states, position_ids)
-    #
-    #     all_hidden_states = () if output_hidden_states else None
-    #     all_self_attns = () if output_attentions else None
-    #     next_decoder_cache = None
-    #
-    #     if memory_prompt is not None:
-    #         memory_prompt = memory_prompt.view(self.config.num_memory_layers, -1, self.config.hidden_size)
-    #         mem_layer_offset = len(self.layers) - self.config.num_memory_layers
-    #
-    #     for i, decoder_layer in enumerate(self.layers):
-    #         if output_hidden_states:
-    #             all_hidden_states += (hidden_states,)
-    #
-    #         current_mem = None
-    #         if memory_prompt is not None and i >= mem_layer_offset:
-    #             current_mem = memory_prompt[i - mem_layer_offset].unsqueeze(0).expand(hidden_states.size(0), -1, -1)
-    #
-    #         layer_outputs = decoder_layer(
-    #             hidden_states,
-    #             attention_mask=causal_mask,
-    #             position_ids=position_ids,
-    #             past_key_value=past_key_values,
-    #             output_attentions=output_attentions,
-    #             use_cache=use_cache,
-    #             cache_position=cache_position,
-    #             position_embeddings=position_embeddings,
-    #             memory_prompt=current_mem,
-    #         )
-    #
-    #         hidden_states = layer_outputs[0]
-    #
-    #         if use_cache:
-    #             next_decoder_cache = layer_outputs[2 if output_attentions else 1]
-    #
-    #         if output_attentions:
-    #             all_self_attns += (layer_outputs[1],)
-    #
-    #     hidden_states = self.norm(hidden_states)
-    #
-    #     if output_hidden_states:
-    #         all_hidden_states += (hidden_states,)
-    #
-    #     next_cache = next_decoder_cache if use_cache else None
-    #
-    #     return BaseModelOutputWithPast(
-    #         last_hidden_state=hidden_states,
-    #         past_key_values=next_cache,
-    #         hidden_states=all_hidden_states,
-    #         attentions=all_self_attns,
-    #     )
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        # retrieve input_ids and inputs_embeds
+        if input_ids is not None and inputs_embeds is not None:
+            raise ValueError("You cannot specify both decoder_input_ids and decoder_inputs_embeds at the same time")
+        elif input_ids is not None:
+            batch_size, seq_length = input_ids.shape
+        elif inputs_embeds is not None:
+            batch_size, seq_length, _ = inputs_embeds.shape
+        else:
+            raise ValueError("You have to specify either decoder_input_ids or decoder_inputs_embeds")
+
+        if self.gradient_checkpointing and self.training:
+            if use_cache:
+                logger.warning_once(
+                    "`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`..."
+                )
+                use_cache = False
+
+        past_key_values_length = 0
+
+        if use_cache:
+            use_legacy_cache = not isinstance(past_key_values, Cache)
+            if use_legacy_cache:
+                past_key_values = DynamicCache.from_legacy_cache(past_key_values)
+            past_key_values_length = past_key_values.get_usable_length(seq_length)
+
+        if position_ids is None:
+            device = input_ids.device if input_ids is not None else inputs_embeds.device
+            position_ids = torch.arange(
+                past_key_values_length, seq_length + past_key_values_length, dtype=torch.long, device=device
+            )
+            position_ids = position_ids.unsqueeze(0).view(-1, seq_length)
+        else:
+            position_ids = position_ids.view(-1, seq_length).long()
+
+        if inputs_embeds is None:
+            inputs_embeds = self.embed_tokens(input_ids)
+
+        if attention_mask is not None and self._attn_implementation == "flash_attention_2" and use_cache:
+            is_padding_right = attention_mask[:, -1].sum().item() != batch_size
+            if is_padding_right:
+                raise ValueError(
+                    "You are attempting to perform batched generation with padding_side='right'"
+                    " this may lead to unexpected behaviour for Flash Attention version of Qwen2. Make sure to "
+                    " call `tokenizer.padding_side  = 'left'` before tokenizing the input. "
+                )
+
+        if self._attn_implementation == "flash_attention_2":
+            # 2d mask is passed through the layers
+            attention_mask = attention_mask if (attention_mask is not None and 0 in attention_mask) else None
+        elif self._attn_implementation == "sdpa" and not output_attentions:
+            # output_attentions=True can not be supported when using SDPA, and we fall back on
+            # the manual implementation that requires a 4D causal mask in all cases.
+            attention_mask = _prepare_4d_causal_attention_mask_for_sdpa(
+                attention_mask,
+                (batch_size, seq_length),
+                inputs_embeds,
+                past_key_values_length,
+            )
+        else:
+            # 4d mask is passed through the layers
+            attention_mask = _prepare_4d_causal_attention_mask(
+                attention_mask,
+                (batch_size, seq_length),
+                inputs_embeds,
+                past_key_values_length,
+                sliding_window=self.config.sliding_window,
+            )
+
+        hidden_states = inputs_embeds
+
+        # decoder layers
+        all_hidden_states = () if output_hidden_states else None
+        all_self_attns = () if output_attentions else None
+        next_decoder_cache = None
+
+        for decoder_layer in self.layers:
+            if output_hidden_states:
+                all_hidden_states += (hidden_states,)
+
+            if self.gradient_checkpointing and self.training:
+                layer_outputs = self._gradient_checkpointing_func(
+                    decoder_layer.__call__,
+                    hidden_states,
+                    attention_mask,
+                    position_ids,
+                    past_key_values,
+                    output_attentions,
+                    use_cache,
+                )
+            else:
+                layer_outputs = decoder_layer(
+                    hidden_states,
+                    attention_mask=attention_mask,
+                    position_ids=position_ids,
+                    past_key_value=past_key_values,
+                    output_attentions=output_attentions,
+                    use_cache=use_cache,
+                )
+
+            hidden_states = layer_outputs[0]
+
+            if use_cache:
+                next_decoder_cache = layer_outputs[2 if output_attentions else 1]
+
+            if output_attentions:
+                all_self_attns += (layer_outputs[1],)
+
+        hidden_states = self.norm(hidden_states)
+
+        # add hidden states from the last decoder layer
+        if output_hidden_states:
+            all_hidden_states += (hidden_states,)
+
+        next_cache = None
+        if use_cache:
+            next_cache = next_decoder_cache.to_legacy_cache() if use_legacy_cache else next_decoder_cache
+
+        if not return_dict:
+            return tuple(v for v in [hidden_states, next_cache, all_hidden_states, all_self_attns] if v is not None)
+        return BaseModelOutputWithPast(
+            last_hidden_state=hidden_states,
+            past_key_values=next_cache,
+            hidden_states=all_hidden_states,
+            attentions=all_self_attns,
+        )
 
 class LlavaQwenForCausalLM(Qwen2ForCausalLM, LlavaMetaForCausalLM):
     config_class = LlavaQwenConfig

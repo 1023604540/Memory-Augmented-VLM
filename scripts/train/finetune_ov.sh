7f3f27105942eb10f82ef968dbc9618ffda117ef
Ziqin Zhang <ziqin.zhang@tum.de>
2025-04-26 11:14:46 +0200

    debug train

--- scripts/train/finetune_ov.sh
+++ scripts/train/finetune_ov.sh
@@ -44,7 +44,7 @@ PORT=12346
 
 ACCELERATE_CPU_AFFINITY=0 torchrun --nproc_per_node="${NUM_GPUS}" --nnodes="${NNODES}" --node_rank="${RANK}" --master_addr="${ADDR}" --master_port="${PORT}" \
     llava/train/train_mem.py \
-    --deepspeed scripts/zero3.json \
+    --deepspeed scripts/zero2.json \
     --model_name_or_path $PREV_STAGE_CHECKPOINT \
     --version $PROMPT_VERSION \
     --data_path /home/hpc/b232dd/b232dd16/LLaVA-OV/scripts/train/sharegpt_train.yaml \

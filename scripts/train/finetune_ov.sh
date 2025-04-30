84a8c4f30a2dde93d2fa8382ad528200cac9bbe5
Ziqin Zhang <ziqin.zhang@tum.de>
2025-04-30 13:25:42 +0200

    change run name

--- scripts/train/finetune_ov.sh
+++ scripts/train/finetune_ov.sh
@@ -28,7 +28,7 @@ echo "BASE_RUN_NAME: ${BASE_RUN_NAME}"
 
 # Stage 2
 PROMPT_VERSION="qwen_1_5"
-RUN_NAME="llava-onevision-0.5b-larimar_videollamb_FAU_newLR_correct"
+RUN_NAME="0.5b_FAU_test"
 # PREV_STAGE_CHECKPOINT="/anvme/workspace/b232dd16-LLaVA-OV/llava-onevision-qwen2-7b-ov" # replace it with your last checkpoint training from single image collection
 PREV_STAGE_CHECKPOINT="/anvme/workspace/b232dd16-LLaVA-OV/llava-onevision-qwen2-0.5b-ov"
 echo "PREV_STAGE_CHECKPOINT: ${PREV_STAGE_CHECKPOINT}"

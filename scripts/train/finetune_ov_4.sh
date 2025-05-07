e23665fe05069b462d53cb96b4eaf5a75924c590
Ziqin Zhang <ziqin.zhang@tum.de>
2025-05-07 16:05:57 +0200

    try again with old LR

--- scripts/train/finetune_ov_4.sh
+++ scripts/train/finetune_ov_4.sh
@@ -75,9 +75,9 @@ srun --mpi=pmix --export=ALL,ACCELERATE_CPU_AFFINITY=0 \
     --save_strategy "steps" \
     --save_steps 200 \
     --save_total_limit 3 \
-    --learning_rate 2e-6 \
-    --memory_transformer_lr 1e-4 \
-    --memory_key_value_lr 1e-4 \
+    --learning_rate 1e-5 \
+    --memory_transformer_lr 1e-3 \
+    --memory_key_value_lr 1e-3 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \

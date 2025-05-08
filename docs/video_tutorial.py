cae7e3acdea8cf8006de0d507c21c676efcebac7
Ziqin Zhang <ziqin.zhang@tum.de>
2025-05-09 00:47:21 +0200

    test checkpoint

--- docs/video_tutorial.py
+++ docs/video_tutorial.py
@@ -24,10 +24,10 @@ from memory import KMeansMemory
 print("load model")
 warnings.filterwarnings("ignore")
 # Load the OneVision model
-# pretrained = "/hkfs/work/workspace/scratch/tum_tyz7686-LLaVA-OV/checkpoints/llava-onevision-0.5b-larimar_videollamb_KIT_position/checkpoint-800"
+pretrained = "/hkfs/work/workspace/scratch/tum_tyz7686-LLaVA-OV/checkpoints/llava-onevision-0.5b-qwen2_KIT_position_8tokens"
 # pretrained = "lmms-lab/llava-onevision-qwen2-0.5b-ov"   # Use this for 7B model
 # pretrained = "/anvme/workspace/b232dd16-LLaVA-OV/llava-onevision-qwen2-0.5b-ov"   # Use this for 7B model
-pretrained = "/hkfs/work/workspace/scratch/tum_tyz7686-LLaVA-OV/checkpoints/llava-onevision-qwen2-0.5b-ov"   # Use this for 7B model
+# pretrained = "/hkfs/work/workspace/scratch/tum_tyz7686-LLaVA-OV/checkpoints/llava-onevision-qwen2-0.5b-ov"   # Use this for 7B model
 model_name = "llava_qwen"
 device = "cuda"
 device_map = "auto"
@@ -121,10 +121,10 @@ def dynamic_load_video(video_path):
 
 print("load video")
 # Load and process video
-# video_path = "docs/needle_32.mp4"
-video_path = "/hkfs/work/workspace/scratch/tum_tyz7686-LLaVA-OV/QJ6sjg7SXOQ.mp4"
+video_path = "docs/needle_32.mp4"
+# video_path = "/hkfs/work/workspace/scratch/tum_tyz7686-LLaVA-OV/QJ6sjg7SXOQ.mp4"
 # video_frames = load_video(video_path, 64)
-video_frames = load_video(video_path, 250)
+video_frames = load_video(video_path, 128)
 print(video_frames.shape) # (16, 1024, 576, 3)
 image_tensors = []
 frames = image_processor.preprocess(video_frames, return_tensors="pt")["pixel_values"].half().cuda()
@@ -136,8 +136,8 @@ image_tensors.append(frames)
 # Prepare conversation input
 conv_template = "qwen_1_5"
 
-question = f"{DEFAULT_IMAGE_TOKEN}\n The screen is split into two sections, and in the small section on the far right, what is the man wearing a hat doing in front of a brown horse?\nA. Walking the horse\nB. Punching towards the camera\nC. Riding the horse\nD. Kneeling down\nE. Extending his palm forward while facing the camera\nAnswer with the option's letter from the given choices directly.\n"
-# question = f"{DEFAULT_IMAGE_TOKEN}\n Describe the content of the video\n"
+# question = f"{DEFAULT_IMAGE_TOKEN}\n The screen is split into two sections, and in the small section on the far right, what is the man wearing a hat doing in front of a brown horse?\nA. Walking the horse\nB. Punching towards the camera\nC. Riding the horse\nD. Kneeling down\nE. Extending his palm forward while facing the camera\nAnswer with the option's letter from the given choices directly.\n"
+question = f"{DEFAULT_IMAGE_TOKEN}\n Describe the content of the video\n"
 conv = copy.deepcopy(conv_templates[conv_template])
 conv.append_message(conv.roles[0], question)
 conv.append_message(conv.roles[1], None)

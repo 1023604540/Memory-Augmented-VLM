4b0fdf8148281eddc1e2b17d731545937d65f860
Ziqin Zhang <ziqin.zhang@tum.de>
2025-05-08 02:32:00 +0200

    debug oom

--- scripts/train/finetune_ov_4.sh
+++ scripts/train/finetune_ov_4.sh
@@ -85,7 +85,7 @@ srun --mpi=pmix --export=ALL,ACCELERATE_CPU_AFFINITY=0 \
     --tf32 True \
     --model_max_length 32768 \
     --gradient_checkpointing False \
-    --dataloader_num_workers 2 \
+    --dataloader_num_workers 0 \
     --lazy_preprocess True \
     --report_to wandb \
     --torch_compile True \

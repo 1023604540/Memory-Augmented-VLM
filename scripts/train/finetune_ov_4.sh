79ac90ad52c66862987cd33a2a42920f185deb56
Ziqin Zhang <ziqin.zhang@tum.de>
2025-05-08 14:18:14 +0200

    test on small dataset

--- scripts/train/finetune_ov_4.sh
+++ scripts/train/finetune_ov_4.sh
@@ -94,7 +94,6 @@ srun --mpi=pmix --export=ALL,ACCELERATE_CPU_AFFINITY=0 \
     --force_sample False \
     --frames_upbound 250 \
     --attn_implementation "flash_attention_2" \
-    --ddp_find_unused_parameters False
 exit 0;
 
 # You can delete the sdpa attn_implementation if you want to use flash attn

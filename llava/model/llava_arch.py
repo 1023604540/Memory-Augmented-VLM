a6c585b2d666c7d2329f1e72c964df63b746a42b
Ziqin Zhang <ziqin.zhang@tum.de>
2025-05-12 14:33:45 +0200

    test checkpoint

--- llava/model/llava_arch.py
+++ llava/model/llava_arch.py
@@ -490,6 +490,7 @@ class LlavaMetaForCausalLM(MultimodalOpsMixin, ABC):
                     if number < dropout_rate:
                         updated_image_segment = torch.zeros(updated_image_segment.shape).to(device=self.device,dtype=self.dtype)
                         rank_print(f"updated_image_segment dropout")
+            updated_image_segment = torch.zeros(updated_image_segment.shape).to(device=self.device, dtype=self.dtype)
             memory_augmented_features.append(updated_image_segment)
             if recurrent_memory is not None:
                 self.get_model().memory_readout_cache = recurrent_memory

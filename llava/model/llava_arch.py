d0ace054f00abc929b1bb8746d780f5809e89519
Ziqin Zhang <ziqin.zhang@tum.de>
2025-05-14 16:51:35 +0200

    delete print

--- llava/model/llava_arch.py
+++ llava/model/llava_arch.py
@@ -807,6 +807,8 @@ class LlavaMetaForCausalLM(MultimodalOpsMixin, ABC):
         # hidden_size = 896
         # memory_prompt = torch.randn(num_memory_layers, memory_length, hidden_size).to(dtype=self.dtype, device=self.device)
         # memory_prompt_stack = torch.rand([10, 27840, 896]).to(dtype=self.dtype, device=self.device)
+
+        ######################### PCA Visualization #########################
         import numpy as np
         import matplotlib
         matplotlib.use('Agg')  # 非交互式后端
@@ -838,8 +840,9 @@ class LlavaMetaForCausalLM(MultimodalOpsMixin, ABC):
             plt.tight_layout()
             plt.savefig(filename)
             print(f"PCA 投影图已保存为 {filename}")
-
         # save_memory_pca(recurrent_model.memory_cache)
+        ######################### PCA Visualization #########################
+
         return memory_prompt_stack, None, position_ids, attention_mask, past_key_values, new_input_embeds, new_labels
 
     def inject_memory_as_kv(self, memory_readout, old_cache=None):

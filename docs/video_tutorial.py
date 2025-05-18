283a388d3bab8668e4e7cf91b6be19f7f35b4a03
Ziqin Zhang <ziqin.zhang@tum.de>
2025-05-19 01:27:55 +0200

    change test checkpoint

--- docs/video_tutorial.py
+++ docs/video_tutorial.py
@@ -138,6 +138,7 @@ conv_template = "qwen_1_5"
 
 # question = f"{DEFAULT_IMAGE_TOKEN}\n The screen is split into two sections, and in the small section on the far right, what is the man wearing a hat doing in front of a brown horse?\nA. Walking the horse\nB. Punching towards the camera\nC. Riding the horse\nD. Kneeling down\nE. Extending his palm forward while facing the camera\nAnswer with the option's letter from the given choices directly.\n"
 question = f"{DEFAULT_IMAGE_TOKEN}\nDescribe what's happening in this video."
+question = f"{DEFAULT_IMAGE_TOKEN}\nIs there a woman in the video? What is she doing?"
 conv = copy.deepcopy(conv_templates[conv_template])
 conv.append_message(conv.roles[0], question)
 conv.append_message(conv.roles[1], None)

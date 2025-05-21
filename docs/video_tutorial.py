e743429bd0e14357ed1477d57593b92d86208602
Ziqin Zhang <ziqin.zhang@tum.de>
2025-05-21 12:51:07 +0200

    change test checkpoint

--- docs/video_tutorial.py
+++ docs/video_tutorial.py
@@ -24,7 +24,7 @@ from memory import KMeansMemory
 print("load model")
 warnings.filterwarnings("ignore")
 # Load the OneVision model
-pretrained = "/anvme/workspace/b232dd16-LLaVA-OV/checkpoints/0.5b_FAU_llava_onevision_qwen2_8tokens_initial"
+pretrained = "/anvme/workspace/b232dd16-LLaVA-OV/checkpoints/0.5b_FAU_llava_onevision_qwen2_8tokens_initial_2ndHalf_2ndEpoch"
 # pretrained = "lmms-lab/llava-onevision-qwen2-0.5b-ov"   # Use this for 7B model
 # pretrained = "/anvme/workspace/b232dd16-LLaVA-OV/llava-onevision-qwen2-0.5b-ov"   # Use this for 7B model
 # pretrained = "/hkfs/work/workspace/scratch/tum_tyz7686-LLaVA-OV/checkpoints/llava-onevision-qwen2-0.5b-ov"   # Use this for 7B model

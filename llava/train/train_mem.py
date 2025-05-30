763aa2763eb07cebc847c2bc9345bf4f08743f4b
Ziqin Zhang <ziqin.zhang@tum.de>
2025-05-30 14:10:11 +0200

    torch_compile False

--- llava/train/train_mem.py
+++ llava/train/train_mem.py
@@ -1,5 +1,9 @@
 import torch._dynamo
 torch._dynamo.config.optimize_ddp = False
+import os
+
+print(f"[RANK={os.environ.get('RANK')}] CUDA_VISIBLE_DEVICES={os.environ.get('CUDA_VISIBLE_DEVICES')}")
+print(f"[RANK={os.environ.get('RANK')}] torch.cuda.device_count()={torch.cuda.device_count()}")
 from llava.train.train import train
 
 if __name__ == "__main__":

05d152c4e47fb1ec822c9046f2e5f6eecfa4d07b
Ziqin Zhang <ziqin.zhang@tum.de>
2025-05-30 14:32:51 +0200

    ddp debug

--- llava/train/train_mem.py
+++ llava/train/train_mem.py
@@ -1,6 +1,7 @@
 # import torch._dynamo
 # torch._dynamo.config.optimize_ddp = False
 import os
+import torch
 
 print(f"[RANK={os.environ.get('RANK')}] CUDA_VISIBLE_DEVICES={os.environ.get('CUDA_VISIBLE_DEVICES')}")
 print(f"[RANK={os.environ.get('RANK')}] torch.cuda.device_count()={torch.cuda.device_count()}")

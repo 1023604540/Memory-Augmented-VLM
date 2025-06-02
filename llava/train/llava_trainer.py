992965310bca132c8d1914b8ac450b38b2bb3836
Ziqin Zhang <ziqin.zhang@tum.de>
2025-06-02 03:02:11 +0200

    use memory_cache, discard updated_image_features, discard sample initialization

--- llava/train/llava_trainer.py
+++ llava/train/llava_trainer.py
@@ -383,6 +383,7 @@ class LLaVATrainer(Trainer):
             if self.args.memory_key_value_lr is not None:
                 # Apply memory_key_value_lr to memory_key_projs and memory_value_projs
                 lr_mapper["token_type_embedding"] = self.args.memory_key_value_lr
+                lr_mapper["memory_fuser"] = self.args.memory_key_value_lr
 
             if len(lr_mapper) > 0:
                 special_lr_parameters = [name for name, _ in opt_model.named_parameters() if any(module_keyword in name for module_keyword in lr_mapper)]

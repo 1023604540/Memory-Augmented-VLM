91a0c2bb5e23b6e4c2761a9ee5918370748a5020
Ziqin Zhang <ziqin.zhang@tum.de>
2025-06-03 17:39:40 +0200

    can resume from interrupted

--- video_reader_2-3m.py
+++ video_reader_2-3m.py
@@ -9,7 +9,7 @@ import multiprocessing
 import time
 
 # =================== CONFIGURATION =====================
-DATA_YAML = "/hkfs/work/workspace/scratch/tum_tyz7686-LLaVA-OV/LLaVA-NeXT/scripts/train/test2-3.yaml"
+DATA_YAML = "/hkfs/work/workspace/scratch/tum_tyz7686-LLaVA-OV/LLaVA-NeXT/scripts/train/temp.yaml"
 VIDEO_FOLDER = "/hkfs/work/workspace/scratch/tum_tyz7686-hf_storage/videos"
 OUTPUT_FOLDER = "/hkfs/work/workspace/scratch/tum_tyz7686-hf_storage/videos_tensors"
 PROCESS_COUNT = 8     # Try 8, 16, 32, 64 -- tune for your node!

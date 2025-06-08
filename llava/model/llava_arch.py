cd33347d714bf97ddf92b90e88207a07c742b2bc
Ziqin Zhang <ziqin.zhang@tum.de>
2025-06-08 11:59:41 +0200

    add positional encoding to trainable parts

--- llava/model/llava_arch.py
+++ llava/model/llava_arch.py
@@ -125,7 +125,7 @@ class LlavaMetaModel:
         self.positional_encoding = TemporalPositionalEncoding(
             max_frames=300,
             embed_dim=LLM_hidden_dim,
-            learnable=False
+            learnable=True
         ).to(self.device)
         self.token_type_embedding = nn.Embedding(2, 896).to(self.device)
     def get_vision_tower(self):

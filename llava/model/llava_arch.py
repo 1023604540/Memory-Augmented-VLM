aaea86bc2b20b9035f9b8ff7d62e4b7b379dd290
Ziqin Zhang <ziqin.zhang@tum.de>
2025-06-09 20:51:48 +0200

    remove gru

--- llava/model/llava_arch.py
+++ llava/model/llava_arch.py
@@ -280,7 +280,7 @@ class LlavaMetaForCausalLM(MultimodalOpsMixin, ABC):
         image_features = self.get_model().get_vision_tower()(images)
         # image_features = self.get_model().vision_resampler(image_features, images=images)
         image_features = self.get_model().mm_projector(image_features)
-        image_features = self.get_model().gru_encoder(image_features)
+        # image_features = self.get_model().gru_encoder(image_features)
         return image_features
 
     def encode_multimodals(self, videos_or_images, video_idx_in_batch, split_sizes=None):

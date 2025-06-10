6f6aa508e18a04616393257da7788088a8296ac0
Ziqin Zhang <ziqin.zhang@tum.de>
2025-06-11 01:46:34 +0200

    lower LR, on short data first

--- scripts/train/finetune_ov_4.sh
+++ scripts/train/finetune_ov_4.sh
@@ -28,7 +28,7 @@ echo "BASE_RUN_NAME: ${BASE_RUN_NAME}"
 
 # Stage 2
 PROMPT_VERSION="qwen_1_5"
-RUN_NAME="llava-onevision-0.5b-qwen2_KIT_position_8tokens_catmemory_adapterOFF"
+RUN_NAME="llava-onevision-0.5b-qwen2_KIT_position_8tokens_catmemory_adapterOFF_short"
 PREV_STAGE_CHECKPOINT="lmms-lab/llava-onevision-qwen2-0.5b-ov" # replace it with your last checkpoint training from single image collection
 echo "PREV_STAGE_CHECKPOINT: ${PREV_STAGE_CHECKPOINT}"
 echo "MID_RUN_NAME: ${RUN_NAME}"
@@ -51,7 +51,7 @@ srun --mpi=pmix --export=ALL,ACCELERATE_CPU_AFFINITY=0 \
     --deepspeed scripts/zero2.json \
     --model_name_or_path $PREV_STAGE_CHECKPOINT \
     --version $PROMPT_VERSION \
-    --data_path /hkfs/work/workspace/scratch/tum_tyz7686-LLaVA-OV/LLaVA-NeXT/scripts/train/long_train.yaml \
+    --data_path /hkfs/work/workspace/scratch/tum_tyz7686-LLaVA-OV/LLaVA-NeXT/scripts/train/short_train.yaml \
     --image_folder /hkfs/work/workspace/scratch/tum_tyz7686-hf_storage/videos \
     --video_folder //hkfs/work/workspace/scratch/tum_tyz7686-hf_storage/videos_tensors \
     --mm_tunable_parts="larimar_model,recurrent_model,mm_language_model" \
@@ -77,9 +77,9 @@ srun --mpi=pmix --export=ALL,ACCELERATE_CPU_AFFINITY=0 \
     --save_strategy "steps" \
     --save_steps 100 \
     --save_total_limit 3 \
-    --learning_rate 1e-5 \
-    --memory_transformer_lr 5e-5 \
-    --memory_key_value_lr 5e-5 \
+    --learning_rate 2e-6 \
+    --memory_transformer_lr 1e-5 \
+    --memory_key_value_lr 1e-5 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \

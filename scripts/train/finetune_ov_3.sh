4f92e302db456a9d0adaf4a71e2268dd5f09e1d5
Ziqin Zhang <ziqin.zhang@tum.de>
2025-06-20 16:03:09 +0200

    -x all

--- scripts/train/finetune_ov_3.sh
+++ scripts/train/finetune_ov_3.sh
@@ -58,11 +58,6 @@ srun --mpi=pmix \
   -x NCCL_DEBUG_SUBSYS \
   -x NCCL_TIMEOUT \
   -x NCCL_P2P_DISABLE \
-  -x WANDB_API_KEY \
-  -x MASTER_ADDR \
-  -x MASTER_PORT \
-  -x RANK \
-  -x NUM_GPUS \
   --export=ALL,ACCELERATE_CPU_AFFINITY=0 \
   torchrun --nproc_per_node="${NUM_GPUS}" --nnodes="${NNODES}" --node_rank="${RANK}" --rdzv_backend=c10d \
     --rdzv_endpoint=${MASTER_ADDR}:${MASTER_PORT} \

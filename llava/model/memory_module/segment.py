2f680a0ad56c6ad76e2642ff78a5655830d9f4dc
Ziqin Zhang <ziqin.zhang@tum.de>
2025-07-01 00:39:57 +0200

    test checkpoint

--- llava/model/memory_module/segment.py
+++ llava/model/memory_module/segment.py
@@ -28,7 +28,7 @@ def segment(features, alpha=0.5, k=None):
     if features.shape[0] == 1:
         return [0], torch.zeros(1)
     print("features shape: ", features.shape)
-    sim_scores = torch.cosine_similarity(features[:-1, :], features[1:, :], eps=1e-3)
+    sim_scores = torch.cosine_similarity(features[:-1, :], features[1:, :], eps=1e-2)
 
     print("sim_scores", sim_scores)
     sim_scores[0] = sim_scores[1]  # Ensure the first score is not zero to avoid division by zero in depth score calculation

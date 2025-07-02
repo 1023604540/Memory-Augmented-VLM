694c0df60fa553d6a8829996996a8128bc242967
Ziqin Zhang <ziqin.zhang@tum.de>
2025-07-02 16:47:30 +0200

    add more academic data

--- scripts/train/finetune_ov_4.sh
+++ scripts/train/finetune_ov_4.sh
@@ -29,7 +29,7 @@ echo "BASE_RUN_NAME: ${BASE_RUN_NAME}"
 
 # Stage 2
 PROMPT_VERSION="qwen_1_5"
-RUN_NAME="KIT_0.5b_qwen2_recurrent_8tokens_catmemory_pe_linear_depth2_2_3_m_academic"
+RUN_NAME="KIT_0.5b_qwen2_recurrent_8tokens_catmemory_pe_linear_depth2_academic_all"
 PREV_STAGE_CHECKPOINT="lmms-lab/llava-onevision-qwen2-0.5b-ov" # replace it with your last checkpoint training from single image collection
 echo "PREV_STAGE_CHECKPOINT: ${PREV_STAGE_CHECKPOINT}"
 echo "MID_RUN_NAME: ${RUN_NAME}"
